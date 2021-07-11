83bffad913d1e1e25339ce7349b473e268b35eae
sumin21 <tnals1178@naver.com>
2021-07-11 23:08:52 +0900

    7/11 develop carlist

--- js/index.js
+++ js/index.js
@@ -1,18 +1,11 @@
-import { Tooltip, Toast, Popover } from 'bootstrap';
+import 'bootstrap';
 import {sum} from './hello.js';
 import {mbclick} from './kk.js';
 import '../scss/style.scss';
-import png from '../logo/logo-carmore@3x.png';
 
 
+import png from '../caricon/car.png';
 
-
-
-window.addEventListener('DOMContentLoaded', () => {
-    const el = document.querySelector('#root');
-    el.innerHTML = `<h1>1+2 = ${sum(1,2)}</h1>`
-    
-})
 
 //document.
 
